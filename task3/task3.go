9b6b5a19d3382e8ecd15120e50ceeb1d90c56c9f
honyshyota <semensuprunenko@gmail.com>
2022-07-20 19:57:01 +0300

    🚀 Complete task 3

---
+++ task3/task3.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	var arr = [5]int{2, 4, 6, 8, 10}
+	fmt.Println(arrayToSquare(arr))
+}
+
+func arrayToSquare(arr [5]int) int {
+	var wg sync.WaitGroup // инициализируем группу ожидания
+	amount := len(arr)
+	wg.Add(amount) // колличество рутин соответствует длине переданного массива
+
+	ch := make(chan int, amount) // инициализируем канал длиной равной длине массива
+
+	for _, val := range arr {
+		go func(val int) { // явная передача аргумента
+			defer wg.Done() // отложенный вызов завершения ожидания
+			ch <- val * val
+		}(val)
+	}
+
+	wg.Wait()
+	close(ch)                                  // после завершения ожидания очереди закрываем канал
+	result := <-ch + <-ch + <-ch + <-ch + <-ch // считываем данные из канала и суммируем их
+	return result
+}
