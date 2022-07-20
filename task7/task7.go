bef462777ed381f170f46bb9984dbce5740f5247
honyshyota <semensuprunenko@gmail.com>
2022-07-21 01:43:14 +0300

    🚀 Complete task 7

---
+++ task7/task7.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	a := make(map[int]int) // инициализируем мапу
+	mu := &sync.Mutex{} // инициализируем мютекс
+	var wg sync.WaitGroup // инициализируем вэйт группу
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(a map[int]int, i int, mu *sync.Mutex) {
+			for j := 0; j < 10; j++ {
+				mu.Lock() // мютекс лочится при работе с мапой и по завершению анлок для других гоурутин
+				a[i*10+j] = j // при записи в мапу нужен уникальный ключ ибо данные при одинаковом ключе перезаписываются
+				mu.Unlock() // тот самый анлок
+			}
+			wg.Done()
+		}(a, i, mu)
+	}
+	wg.Wait()
+	fmt.Println(a)
+}
