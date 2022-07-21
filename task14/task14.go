ba0dedd4ae8101072f494e5c9adb6f1e6797cd93
honyshyota <semensuprunenko@gmail.com>
2022-07-21 18:51:31 +0300

    🚀 Complete task 14

---
+++ task14/task14.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	// объявляем переменный с дефолтными значениями
+	var i int
+	var s string
+	var b bool
+	var ch chan int
+
+	//вызываем нашу функция для опеределения типа переменной и передаем ей заранее заготовленные переменные
+	do(i)
+	do(s)
+	do(b)
+	do(ch)
+
+}
+
+func do(i interface{}) {
+	// используем switch type для определения типа переменной в интерфейсе 
+	switch val := i.(type) {
+	case int:
+		fmt.Println("value is int ", val)
+	case string:
+		fmt.Println("value is string ", val)
+	case bool:
+		fmt.Println("value is boolean ", val)
+	case chan int:
+		fmt.Println("value is channel ", val)
+	}
+}
