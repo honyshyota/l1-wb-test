fcdc9baf94302b28ceb71fb65126e4aa954e5e05
honyshyota <semensuprunenko@gmail.com>
2022-07-21 14:31:07 +0300

    🚀 Complete task 8

---
+++ task8/task8.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+var number int
+var positionBit int
+var variable int
+
+func init() {
+	flag.IntVar(&number, "n", 400, "number")
+	flag.IntVar(&positionBit, "p", 1, "postion bit")
+	flag.IntVar(&variable, "v", 1, "variable value")
+}
+
+func main() {
+	flag.Parse() // парсим аргументы командной строки
+
+	intToBin := strconv.FormatInt(int64(number), 2) // перевод числа в двоичную систему просто для проверки на несоответствие длины
+
+	if positionBit > len(intToBin) {
+		log.Fatalln("позиция бита выходит за рамки представления искомого числа в двоичном исчеслении")
+	} else if positionBit < 0 {
+		log.Fatalln("значение позиции бита должно быть положительным")
+	}
+
+	if variable > 1 || variable < 0 { // проведрка значения бита 1 или 0
+		log.Fatalln("значение бита должно быть либо 1, либо 0")
+	}
+
+	fmt.Printf("Число %d в двоичной системе измерений до изменения %b\n", number, number)
+
+	switch variable {
+	case 1: // используем подразрядную дизюнкцию
+		number |= 1 << positionBit
+	case 0: // используем сброс бита
+		number &^= 1 << positionBit
+	}
+
+	fmt.Printf("Число %d в двоичной системе измерений после изменения %b\n", number, number)
+}
