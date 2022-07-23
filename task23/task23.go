09e1896e8100760790384c40508a4940408a2608
honyshyota <semensuprunenko@gmail.com>
2022-07-23 03:36:04 +0300

    🚀 Complete task 23

---
+++ task23/task23.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var i int
+	flag.IntVar(&i, "i", 3, "delete slice element")
+	flag.Parse()
+
+	arr := []int{0, 1, 2, 3, 4, 5}
+
+	arr[i] = arr[len(arr)-1]
+	arr = arr[:len(arr)-1]
+
+	fmt.Println("Удаление с нарушением сортировки: ", arr)
+
+	arr = []int{0, 1, 2, 3, 4, 5}
+	var res []int
+
+	for index := range arr {
+		if i != index {
+			res = append(res, arr[index])
+		}
+	}
+
+	fmt.Println("Удаление без нарушения сортировки, но через еще один слайс: ", res)
+}
